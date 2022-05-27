4814ac51905ceba3b246aedfefded987fe6cc689
RudhraHimalasya <79926737+RudhraHimalasya@users.noreply.github.com>
2022-05-27 10:12:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-dog= 0;
-cat= 0;
-bn = 0;
-lion = 0;
+// dog= 0;
+// cat= 0;
+// bn = 0;
+// lion = 0;
 
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio:true});
@@ -24,26 +24,26 @@ function gotResults(error, results){
         random_number_g = Math.floor(Math.random()* 255) + 1;
         random_number_b = Math.floor(Math.random()* 255) + 1;
 
-        document.getElementById("result_label").innerHTML = results[0].label;
+        document.getElementById("heading3").innerHTML = results[0].label;
         document.getElementById("result_confidence").innerHTML = (results[0].confidence * 100).toFixed(2) + "%";
-        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g +","+random_number_b+")";
+        document.getElementById("heading3").style.color = "rgb("+random_number_r+","+random_number_g +","+random_number_b+")";
         document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g +","+random_number_b+")";
 
         img1 = document.getElementById('dog');
         img2 = document.getElementById('Cat');
         img3 = document.getElementById('Lion');
-        img4 = document.getElementById('background Noise'); 
+        img4 = document.getElementById('background_Noise'); 
         
         if(results[0].label == "dog"){
             img.src = 'dog.JPG';
         } 
-        if(results[0].label == "Cat"){
+        if(results[0].label == "cat"){
             img.src = 'Cat.JPG';
         } 
-        if(results[0].label == "Lion"){
+        if(results[0].label == "lion"){
             img.src = 'Lion.JPG';
         } 
-        if(results[0].label == "background Noise"){
+        if(results[0].label == "background_Noise"){
         img.src = 'bn.JPG';
         }                
       } 
